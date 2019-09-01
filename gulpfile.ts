eb8aaf355eb4eb398a9c8a237112220605b1a92f
Aryan Naraghi <aryan.naraghi@gmail.com>
2019-09-01 10:05:52 -0700

    Add minification of the HTML sources

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,6 +4,7 @@ const connect = require("gulp-connect");
 const del = require("del");
 const glob = require("glob");
 const gulp = require("gulp");
+const htmlmin = require("gulp-htmlmin");
 const source = require("vinyl-source-stream");
 const sourcemaps = require("gulp-sourcemaps");
 const tsify = require("tsify");
@@ -22,8 +23,8 @@ gulp.task("connect", function() {
   connect.server({
     root: "dist",
     port: 8080,
-    host: '0.0.0.0',
-    livereload: true,
+    host: "0.0.0.0",
+    livereload: true
   });
 });
 
@@ -48,9 +49,26 @@ function compileJs() {
   return buf.pipe(gulp.dest("dist"));
 }
 
-gulp.task("copy-static-files", function() {
-  return gulp.src(["src/*.html", "src/*.jpg"]).pipe(gulp.dest("dist"));
+gulp.task("copy-html", function() {
+  let src = gulp.src(["src/*.html"]);
+
+  if (process.env.PROD) {
+    src = src.pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true
+      })
+    );
+  }
+  return src.pipe(gulp.dest("dist"));
 });
+
+gulp.task("copy-images", function() {
+  return gulp.src(["src/*.jpg"]).pipe(gulp.dest("dist"));
+});
+
+gulp.task("copy-static-files", gulp.series("copy-html", "copy-images"));
 
 gulp.task("clean", function() {
   return del("dist");
@@ -58,6 +76,9 @@ gulp.task("clean", function() {
 
 gulp.task("build", gulp.series("clean", "copy-static-files", compileJs));
 
-gulp.task("load", gulp.parallel(function() { 
-  return watch("src/*", compileJs);
-}, "connect"));
+gulp.task(
+  "load",
+  gulp.parallel(function() {
+    return watch("src/*", compileJs);
+  }, "connect")
+);
